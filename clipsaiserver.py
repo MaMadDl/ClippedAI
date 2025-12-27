d5a2853a81050d95a7f76f6e88be1173af902fcd
Mohammad Ali Gholami <mamaddl@live.com>
2025-12-27 17:04:15 +0330

    Fix import issue

--- clipsaiserver.py
+++ clipsaiserver.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
-from main import task_queue
+from main import task_queue, results_store
+
+import uuid
 
 app = Flask(__name__)
+
 
 @app.route("/run", methods=["POST"])
 def enqueue():
